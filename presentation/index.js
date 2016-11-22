f37fc41b6903e519f6cd5d040d434203ac79fe2d
James Gorman <jamesgorman2@gmail.com>
2016-11-22 21:35:23 +1100

    Intro sections done

--- presentation/index.js
+++ presentation/index.js
@@ -18,7 +18,12 @@ import {
   Markdown,
   Quote,
   Slide,
+  S,
   Spectacle,
+  Table,
+  TableRow,
+  TableItem,
+  TableHeaderItem,
   Text
 } from "spectacle";
 
@@ -46,124 +51,140 @@ const images = {
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#FFAA44"
 });
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+        <Deck transition={["slide"]} progress='number'>
+          <Slide bgColor="primary">
+            <Heading size={1} fit textColor="black">
+              A Practical Introduction to
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
+            <Heading size={1} fit lineHeight={1.5} textColor="black">
+              RxJava
             </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
+            <Link href="https://github.com/jamesgorman2">
+              <Text bold caps textColor="tertiary">James Gorman</Text>
             </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
 
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
+          <Slide>
+            <Heading size={2}>
+              Overview
             </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
             <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
+              <ListItem>What are Reactive Extensions?</ListItem>
+              <ListItem>Why did we choose Rx?</ListItem>
+              <ListItem>Operating on streams</ListItem>
+              <ListItem>Concurrency</ListItem>
+              <ListItem>RxNetty</ListItem>
+              <ListItem>Streaming data</ListItem>
+              <ListItem>RxJava 2</ListItem>
             </List>
           </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
+          <Slide>
+            <Heading size={2}>
+              What are Reactive Extensions?
             </Heading>
-            <Interactive/>
+            <List>
+              <Appear><ListItem>A tool for reactive programming</ListItem></Appear>
+              <Appear><ListItem>Originally from Microsoft labs</ListItem></Appear>
+              <Appear><ListItem>Now polyglot...</ListItem></Appear>
+            </List>
+            <Appear><Text>Java JavaScript C#
+Scala
+Clojure
+C++
+Lua
+Ruby
+Python
+Groovy
+JRuby
+Kotlin
+Swift
+PHP
+</Text></Appear>
           </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
+          <Slide notes="responsive resilient elastic message-driven<br/>Rx = Observables + LINQ + Schedulers\n">
+            <Heading size={2}>
+              Reactive Programming
             </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+            <List>
+              <Appear><ListItem>Popularised with the Reactive Manifesto...</ListItem></Appear>
+              <Appear><ListItem>Badly described by Microsoft...</ListItem></Appear>
+              <Appear><ListItem>Wikipedia...</ListItem></Appear>
+              <Appear><ListItem>asynchnonous programming using observables</ListItem></Appear>
+              <Appear><ListItem>...getting closer</ListItem></Appear>
+              <Appear><ListItem>Programming with asynchronous* data streams**</ListItem></Appear>
+            </List>
+            <Appear>
+              <div>
+                <Text>* sometimes</Text>
+                <Text>** Andre Staltz</Text>
+              </div>
+            </Appear>
+          </Slide>
+          <Slide notes="whole bunch of other similar paridigms - actors, scalaz, spark">
+            <Heading size={2}>
+              OK, so hang on, what is Rx?
+            </Heading>
+            <Table>
+              <TableRow>
+                <TableHeaderItem></TableHeaderItem>
+                <TableHeaderItem>Single</TableHeaderItem>
+                <TableHeaderItem>Collection</TableHeaderItem>
+              </TableRow>
+              <TableRow>
+                <TableHeaderItem>Pull</TableHeaderItem>
+                <TableItem>Object&lt;T&gt;</TableItem>
+                <TableItem>List&lt;T&gt;</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableHeaderItem><Appear><div>Push</div></Appear></TableHeaderItem>
+                <TableItem><Appear><div>Future&lt;T&gt;</div></Appear></TableItem>
+                <TableItem><Appear><div>Observable&lt;T&gt;</div></Appear></TableItem>
+              </TableRow>
+            </Table>
+          </Slide>
+          <Slide>
+            <Heading size={2}>
+              Why did we choose Rx?
+            </Heading>
+            <List>
+              <ListItem>We are in a polyglot environment</ListItem>
+              <ListItem>Dealing with moving data</ListItem>
+              <ListItem>From unreliable clients</ListItem>
+              <ListItem>Rx* provides a functional-lite way to write code</ListItem>
+            </List>
+            <Text><S type="bold">There are now major differences between different implementations of Rx</S></Text>
+        </Slide>
+          <Slide>
+            <Heading size={2}>
+              Operating on streams
+            </Heading>
+          </Slide>
+          <Slide>
+            <Heading size={2}>
+              Concurrency
+            </Heading>
+          </Slide>
+          <Slide>
+            <Heading size={2}>
+              RxNetty
+            </Heading>
+          </Slide>
+          <Slide>
+            <Heading size={2}>
+              Streaming data
+            </Heading>
+          </Slide>
+          <Slide>
+            <Heading size={2}>
+              RxJava 2
+            </Heading>
           </Slide>
         </Deck>
       </Spectacle>
